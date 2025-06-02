0da69a205b3d7ef0ccd2f0bacd88d4207c02f7e6
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-06-02 13:35:34 +0900

    add: go router

--- flutter/courses/udemy/flutter-zero-to-mastery/todo-list/lib/presentation/app/app.dart
+++ flutter/courses/udemy/flutter-zero-to-mastery/todo-list/lib/presentation/app/app.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:todo/presentation/core/routes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      title: 'ToDo App',
+      localizationsDelegates: [
+        ...GlobalMaterialLocalizations.delegates,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      themeMode: ThemeMode.system,
+      theme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.light,
+        ),
+      ),
+      darkTheme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      routerConfig: routes,
+    );
   }
 }
