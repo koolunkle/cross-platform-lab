c8cabb9492a96ef170ad3fe48112c1e59bf6bef9
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-06-04 23:18:24 +0900

    add: Firestore remote data source - collection

--- flutter/courses/udemy/flutter-zero-to-mastery/todo-list/lib/data/exceptions/exceptions.dart
+++ flutter/courses/udemy/flutter-zero-to-mastery/todo-list/lib/data/exceptions/exceptions.dart
@@ -1,3 +1,10 @@
+class ServerException implements Exception {}
+
+class FirestoreCollectionNotFoundException implements ServerException {
+  final String id;
+
+  FirestoreCollectionNotFoundException({required this.id});
+}
 
 class CacheException implements Exception {}
 
