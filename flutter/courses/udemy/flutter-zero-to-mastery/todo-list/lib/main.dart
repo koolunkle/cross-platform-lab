475e73cc5c076e5aa81e66d6032c10d5f644b19a
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-06-04 18:21:34 +0900

    add: auth cubit

--- flutter/courses/udemy/flutter-zero-to-mastery/todo-list/lib/main.dart
+++ flutter/courses/udemy/flutter-zero-to-mastery/todo-list/lib/main.dart
@@ -1,18 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todo/data/local/hive_local_datasource.dart';
 import 'package:todo/data/repositories/todo_repository_local.dart';
 import 'package:todo/domain/repositories/todo_repository.dart';
+import 'package:todo/firebase_options.dart';
 import 'package:todo/presentation/app/app.dart';
+import 'package:todo/presentation/app/cubit/auth_cubit.dart';
 
 Future<void> main() async {
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   final localDataSource = HiveLocalDataSource();
   await localDataSource.init();
+
+  final authCubit = AuthCubit();
+
+  FirebaseAuth.instance.authStateChanges().listen((user) {
+    debugPrint('user: $user');
+    authCubit.authStateChanged(user: user);
+  });
+
   runApp(
     RepositoryProvider<ToDoRepository>(
       create:
           (context) => ToDoRepositoryLocal(localDataSource: localDataSource),
-      child: const App(),
+      child: BlocProvider(create: (context) => authCubit, child: const App()),
     ),
   );
 }
